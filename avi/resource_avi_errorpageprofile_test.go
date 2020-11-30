2057ac2b52fc4ffa39312e572dd4ad3c653a8159
Yograj Shisode <yograjshisode@avinetworks.com>
2020-11-30 09:17:05 +0000

    AV-101124 Added error_pages in error page profile

--- avi/resource_avi_errorpageprofile_test.go
+++ avi/resource_avi_errorpageprofile_test.go
@@ -95,6 +95,14 @@ data "avi_tenant" "default_tenant"{
 }
 resource "avi_errorpageprofile" "testErrorPageProfile" {
 	name = "test-epp-abc"
+	error_pages {
+	index = "1"
+	enable = false
+	match {
+		match_criteria = "IS_IN"
+		status_codes = ["400","404"]
+	}
+}
 	tenant_ref = data.avi_tenant.default_tenant.id
 }
 `
@@ -105,6 +113,14 @@ data "avi_tenant" "default_tenant"{
 }
 resource "avi_errorpageprofile" "testErrorPageProfile" {
 	name = "test-epp-updated"
+	error_pages {
+	index = "1"
+	enable = false
+	match {
+		match_criteria = "IS_IN"
+		status_codes = ["400","401","404"]
+	}
+}
 	tenant_ref = data.avi_tenant.default_tenant.id
 }
 `
